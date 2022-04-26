35d9f75857cfaeb9dac1ece20d9fa1c298ac9b5f
Bartlomiej Kopysc <bkopysc@wp.pl>
2022-04-26 16:41:48 +0200

    navbar fixed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import {Box}  from '@chakra-ui/react';
 import Navbar from './components/Navbar';
 import React, { useState, useEffect } from "react";
 import { Routes, Route, Link } from "react-router-dom";
@@ -8,12 +9,12 @@ import Home from './components/Home';
 function App() {
   return (
     <div className="App">
-      <Navbar></Navbar>
-      <div className="content">
+      <Navbar/>
+      <Box className="content" pt={5}>
         <Routes>
           <Route path="/" element={<Home/>} />
         </Routes>
-      </div>
+      </Box>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,96 @@
+import { ReactNode } from 'react';
+import { MdMenu, MdClose, MdAdd } from "react-icons/md"
+import {
+    Heading,
+    Text,
+    Box,
+    Flex,
+    Avatar,
+    HStack,
+    Link,
+    IconButton,
+    Button,
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuItem,
+    MenuDivider,
+    useDisclosure,
+    useColorModeValue,
+    Stack,
+    textDecoration,
+} from '@chakra-ui/react';
 
-function Navbar(){
-    return(
-        <div>
-            <h3>Navbar</h3>
-        </div>
+const Links = [];
+
+function Navbar(props) {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const handleToggle = () => (isOpen ? onClose() : onOpen());
+
+    return (
+        <Flex
+            as="nav"
+            align="center"
+            justify="space-between"
+            wrap="wrap"
+            padding={4}
+            bg="teal.500"
+            color="white"
+            {...props}
+        >
+            <Flex align="center" mr={5}>
+                    <Heading as="h1" size="md" letterSpacing={"tighter"}>
+                        <a href="/">MyShelf</a>
+                    </Heading>
+            </Flex>
+
+            <Box display={{ base: "block", md: "none" }} onClick={handleToggle}>
+                <MdMenu />
+            </Box>
+
+            <Stack
+                direction={{ base: "column", md: "row" }}
+                display={{ base: isOpen ? "block" : "none", md: "flex" }}
+                width={{ base: "full", md: "auto" }}
+                ml={{base:0, md:6}}
+                alignItems="center"
+                flexGrow={1}
+                mt={{ base: 4, md: 0 }}
+                spacing={{base:3, md:7}}
+                mb={{ base:5, md:0}}
+            >
+                <Box _hover={{textDecoration: "underline"}}>
+                <a href="/docs" >Docs</a>
+                </Box>
+                <Text>Examples</Text>
+                <Text>Blog</Text>
+            </Stack>
+
+            <Stack
+                direction={{ base: "column", md: "row" }}
+                display={{ base: isOpen ? "block" : "none", md: "flex" }}
+                alignItems="center"
+                width={{ base: "full", md: "auto" }}
+                spacing={{base:3, md:3}}
+            >
+                <hr/>
+                <Button
+                    variant="outline"
+                    width={"100px"}
+                    _hover={{ bg: "teal.700", borderColor: "teal.700" }}
+                >
+                    Sign up
+                </Button>
+                <br/>
+                <Button
+                    variant="outline"
+                    width={"100px"}
+                    _hover={{ bg: "teal.700", borderColor: "teal.700" }}
+                >
+                    Login
+                </Button>
+            </Stack>
+        </Flex>
     );
-}
-
+};
 export default Navbar;
